ee20415a30e51ce8b5362e0466f009731cebec09
maoshuyu <maoshuyu@gmail.com>
2013-01-24 16:00:48 +0800

    解决rss/index.js在git上缩进的问题。

--- routes/rss/index.js
+++ routes/rss/index.js
@@ -7,35 +7,35 @@ var data2xml = require('data2xml')()
 
 exports.index = function(req, res) {
     var opt = { 
-	        'limit': rssConfig.maxRssItem, 
-		    'sort': {'createTime': 'desc'}
-	    }
+            'limit': rssConfig.maxRssItem, 
+            'sort': {'createTime': 'desc'}
+        }
       , fields = ['_id', 'title', 'content', 'createTime'];
     articleCtrl.getArticleByQuery({}, fields, opt, function(err, articles) {
-	    if (err) {			
-		    return next(err);	
-	    }
-	    var rssObj = {
-		    '_attr': { version: '2.0' },
-		    'channel': {
-			    'title': rssConfig.title,	
-			    'link': rssConfig.link,
-			    'description': rssConfig.description,
-			    'language': rssConfig.language,
-			    'item': []
-		    }
-	    };
-	    _.each(articles, function(article, index) {
-		    rssObj.channel.item.push({
-			    'title': article.title,	
-			    'link': rssConfig.link + '#article' + article._id,
-			    'guid': rssConfig.link + '#article' + article._id,
-			    'description': markdown.toHTML(article.content),
-			    'author': rssConfig.author, 
-			    'pubDate': article.createTime
-		    });
-	    });
-	    res.contentType('application/xml');
-	    res.send(data2xml('rss', rssObj));
+        if (err) {			
+            return next(err);	
+        }
+        var rssObj = {
+            '_attr': { version: '2.0' },
+            'channel': {
+                'title': rssConfig.title,	
+                'link': rssConfig.link,
+                'description': rssConfig.description,
+                'language': rssConfig.language,
+                'item': []
+            }
+        };
+        _.each(articles, function(article, index) {
+            rssObj.channel.item.push({
+                'title': article.title,	
+                'link': rssConfig.link + '#article' + article._id,
+                'guid': rssConfig.link + '#article' + article._id,
+                'description': markdown.toHTML(article.content),
+                'author': rssConfig.author, 
+                'pubDate': article.createTime
+            });
+        });
+        res.contentType('application/xml');
+        res.send(data2xml('rss', rssObj));
     });
 }
