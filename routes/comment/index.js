0aaa29113104b803558d9e64d5cf7c4574feb9e0
maoshuyu <maoshuyu@gmail.com>
2013-01-24 15:01:15 +0800

    解决有些js文件缩进乱的问题

--- routes/comment/index.js
+++ routes/comment/index.js
@@ -4,58 +4,57 @@ var models = require('../../models')
 
 
 function addComment(newComment, cb) {
-	var comment = new Comment(newComment);
-	comment.save(function(err, comment) {
-		if (err) {
-			return cb(err);	
-		}
-		//更新article的commentCount
-		articleCtrl.getArticleById(comment.articleId, function(err, article) {
-			if (err) {
-				cb(err);	
-			}
-			article.commentCount += 1;
-			article.save();
-		});
-		cb(null, comment);
-	});
+    var comment = new Comment(newComment);
+    comment.save(function(err, comment) {
+	    if (err) {
+		    return cb(err);	
+	    }
+	    //更新article的commentCount
+	    articleCtrl.getArticleById(comment.articleId, function(err, article) {
+		    if (err) {
+			    cb(err);	
+		    }
+		    article.commentCount += 1;
+		    article.save();
+	    });
+	    cb(null, comment);
+    });
 }
 
 function getCommentsByArticleId(id, cb) {
-	Comment.find({'articleId': id}, [], {sort: {'createTime': 'asc'}}, function(err, list) {
-		if (err) {
-			cb(err);
-		}
-		cb(null, list);
-	});
+    Comment.find({'articleId': id}, [], {sort: {'createTime': 'asc'}}, function(err, list) {
+	    if (err) {
+		    cb(err);
+	    }
+	    cb(null, list);
+    });
 }
 
 exports.save = function(req, res) {
-	var content = req.body.content,
-	name = req.body.name,
-	email = req.body.email,
-	home = req.body.home,
-	articleId = req.body.articleId;
-	addComment({
-		'articleId': articleId,	
-		'content': content,
-		'name': name,
-		'email': email,
-		'home': home
-	}, function(err, comment) {
-		if (err) {
-			return next(err);	
-		}
-		res.json(comment);
-	});
-
+    var content = req.body.content,
+    name = req.body.name,
+    email = req.body.email,
+    home = req.body.home,
+    articleId = req.body.articleId;
+    addComment({
+	    'articleId': articleId,	
+	    'content': content,
+	    'name': name,
+	    'email': email,
+	    'home': home
+    }, function(err, comment) {
+	    if (err) {
+		    return next(err);	
+	    }
+	    res.json(comment);
+    });
 };
 exports.list = function(req, res) {
-	var id = req.params.articleId;
-	getCommentsByArticleId(id, function(err, list) {
-		if (err) {
-			return next(err);	
-		}
-		res.json(list);
-	});
+    var id = req.params.articleId;
+    getCommentsByArticleId(id, function(err, list) {
+	    if (err) {
+		    return next(err);	
+	    }
+	    res.json(list);
+    });
 }
