fbef6de6986c62e887ebe28a16919eead81a2761
maoshuyu <maoshuyu@gmail.com>
2013-01-24 14:52:04 +0800

    comment.js缩进的问题

--- models/comment.js
+++ models/comment.js
@@ -3,8 +3,8 @@ var mongoose = require('mongoose')
   , ObjectId = Schema.ObjectId;
 
 var CommentSchema = new Schema({
-	'content': { type: String },
-	'articleId': { type: ObjectId, index: true },
+    'content': { type: String },
+    'articleId': { type: ObjectId, index: true },
     'createTime': { type: Date, default: Date.now },
     'name': { type: String },
     'email': { type: String },
