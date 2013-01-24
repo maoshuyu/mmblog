b4979defdc9c55b426bbf4ea9cb2d49a345e938b
maoshuyu <maoshuyu@gmail.com>
2013-01-25 01:43:54 +0800

    修改config.js在github下缩进的问题

--- config.js
+++ config.js
@@ -1,25 +1,25 @@
 exports.config = {
-	'name': 'MaoMao\' Blog',	
-	'brand': 'MaoMao\' Blog',	
-	'description': '',
+    'name': 'MaoMao\' Blog',	
+    'brand': 'MaoMao\' Blog',	
+    'description': '',
 
-	'author': {
-		'name': '毛毛',
-		'head': '/res/head.jpg',
-		'description': '2011年毕业于河北工程大学地理信息系统专业，目前就职于人人网FED。'
-	},
+    'author': {
+        'name': '毛毛',
+        'head': '/res/head.jpg',
+        'description': '2011年毕业于河北工程大学地理信息系统专业，目前就职于人人网FED。'
+    },
 
-	'domain': 'http://localhost:3000',
-	//静态文件存储的URL
-	'staticUrl': 'http://localhost:3000',
-	'db': 'mongodb://127.0.0.1/mmblog',
+    'domain': 'http://localhost:3000',
+    //静态文件存储的URL
+    'staticUrl': 'http://localhost:3000',
+    'db': 'mongodb://127.0.0.1/mmblog',
 
-	'rss': {
-		'title': 'MaoMao\' Blog', 
-		'link': 'http://localhost:3000',
-		'language': 'zh-cn',
-  		'description': '',
-  		'author': '毛毛',
-  		'maxRssItem': 50 
-	}
+    'rss': {
+        'title': 'MaoMao\' Blog', 
+        'link': 'http://localhost:3000',
+        'language': 'zh-cn',
+        'description': '',
+        'author': '毛毛',
+        'maxRssItem': 50 
+    }
 };
