61473c363629b0bde4e2a59771d7a99258c6a4c1
maoshuyu <maoshuyu@gmail.com>
2013-02-16 22:08:26 +0800

    config增加注释

--- config.js
+++ config.js
@@ -1,26 +1,33 @@
 exports.config = {
+
     'name': 'MaoMao\' Blog',	
     'brand': 'MaoMao\' Blog',	
     'description': '',
 
+    //favicon 
     'favicon': './public/favicon.ico',
 
+    //BLOG作者
     'author': {
         'name': '毛毛',
         'head': 'http://s.maoshuyu.com/res/head.jpg',
         'description': '2011年毕业于河北工程大学地理信息系统专业，目前就职于人人网FED。'
     },
 
+    //域名
     'domain': 'http://blog.maoshuyu.com',
     //静态文件存储的URL
     'staticUrl': 'http://s.maoshuyu.com',
+    //数据库
     'db': 'mongodb://127.0.0.1/mmblog',
 
+    //log
     'log': {
         'access': './log/access.log', 
         'error': './log/error.log'
     },
 
+    //RSS配置信息
     'rss': {
         'title': 'MaoMao\' Blog', 
         'link': 'http://blog.maoshuyu.com',
